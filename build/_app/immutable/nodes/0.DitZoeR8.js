56fc3f6bed11a8c3793f2e164e5ef56be559e5aa
アリフリュウ <zoelabbb@gmail.com>
2025-04-24 20:52:19 +0700

    feat: add static adapter for SvelteKit and update project structure
    - Introduced `@sveltejs/adapter-static` for static site generation.
    - Updated `svelte.config.js` to configure the static adapter with appropriate options.
    - Added new entry files for Svelte components and nodes.
    - Created version.json to track application version.
    - Added favicon image to the project.
    - Updated bun.lock and package.json to include new dependencies.
    - Implemented logic for the Hangman game with improved state management and UI components.

---
+++ build/_app/immutable/nodes/0.DitZoeR8.js
@@ -0,0 +1,1 @@
+import{c as s,a as f}from"../chunks/C81HnwRw.js";import{b as i,E as p,f as c,w as d,x as m,h,g as l,y as _}from"../chunks/malqDCfq.js";function u(t,e,...a){var n=t,o=d,r;i(()=>{o!==(o=e())&&(r&&(m(r),r=null),r=c(()=>o(n,...a)))},p),h&&(n=l)}function E(t,e){var a=s(),n=_(a);u(n,()=>e.children),f(t,a)}export{E as component};

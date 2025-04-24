81527a19fb9057156c5d9196fd6cffe45584ed9b
アリフリュウ <zoelabbb@gmail.com>
2025-04-24 21:01:13 +0700

    feat: Update Svelte adapter to auto and modify .gitignore to include build directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,19 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(
-			{
-				// default options are shown. On some platforms
-				// these options are set automatically — consult the adapter documentation for advice
-				pages: 'build',
-				assets: 'build',
-				fallback: null,
-				strict: false
-			}
-		),
-		paths: {
-			base: '/hangman-svelte'
-		}
+		adapter: adapter()
 	}
 };
 
